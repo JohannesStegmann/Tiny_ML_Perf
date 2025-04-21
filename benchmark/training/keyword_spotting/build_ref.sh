b6bb6894b0fe3b9227992b899426e8132ed541ed
Johannes_Stegmann <joe.a.stegmann@gmail.com>
2025-04-21 14:33:12 +0200

    Commit for transfer

--- benchmark/training/keyword_spotting/build_ref.sh
+++ benchmark/training/keyword_spotting/build_ref.sh
@@ -3,7 +3,7 @@ export TF_CPP_MIN_LOG_LEVEL=2
 
 
 
-python train.py --epochs=10 --run_test_set=True 
+python train.py --epochs=30 --run_test_set=True 
 python mk_cal_set.py
 python quantize.py  --tfl_file_name=trained_models/kws_model.tflite 
 # python quantize_custom.py
--- benchmark/training/keyword_spotting/quantize_custom.py
+++ benchmark/training/keyword_spotting/quantize_custom.py
@@ -1,4 +1,8 @@
 import keras_model as models
 import kws_util
 
-kws_util.get_da
+Flags, unparsed = kws_util.parse_command()
+
+model_settings = models.prepare_model_settings(12,Flags)
+
+print(model_settings)
